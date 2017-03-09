a9204cb2db50d55cc884de2b0f0ead83c8d70dea
holymoly <holzmolz@googlemail.com>
2017-03-09 06:53:38 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -2,7 +2,6 @@ GLUON_SITE_PACKAGES := \
   gluon-mesh-batman-adv-15 \
   gluon-alfred \
   gluon-respondd \
-  ffho-ath9k-blackout-workaround \
   gluon-autoupdater \
   gluon-config-mode-autoupdater \
   gluon-config-mode-contact-info \
