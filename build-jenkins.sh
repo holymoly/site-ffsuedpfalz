fa7761f0bfb81794b82eff46c0066b11849d4e61
holymoly <holzmolz@googlemail.com>
2017-03-09 11:30:22 +0100

    update build config

--- build-jenkins.sh
+++ build-jenkins.sh
@@ -19,26 +19,32 @@ set -e
 
 # Globale Einstellungen
 export GLUON_URL=https://github.com/freifunk-gluon/gluon.git
-export GLUON_COMMIT=v2016.2.3
+export GLUON_COMMIT=v2016.2.2
 export GLUON_RELEASE=$GLUON_COMMIT-`date '+%Y%m%d'`
 
 echo "Building gluon $GLUON_COMMIT -> $GLUON_RELEASE"
 
 # Verzeichnis für Gluon-Repo erstellen und initialisieren
-test -d "/temp/${env.BRANCH_NAME}" || git clone "$GLUON_URL" "/temp/${env.BRANCH_NAME}"
-cd "/temp/${env.BRANCH_NAME}"
+echo  "/temp/$1"
+
+test -d "/temp/$1" || git clone "$GLUON_URL" "/temp/$1"
+cd "/temp/$1"
 git fetch
 git checkout -f $GLUON_COMMIT
 
 # Site config kopieren
-test -d "/temp/${env.BRANCH_NAME}/site" && rm -r "/temp/${env.BRANCH_NAME}/site"
-mkdir "/temp/${env.BRANCH_NAME}/site"
-cp "${WORKSPACE}/modules" "/temp/${env.BRANCH_NAME}/site/"
-cp "${WORKSPACE}/site.mk" "/temp/${env.BRANCH_NAME}/site/"
-cp "${WORKSPACE}/site.conf" "/temp/${env.BRANCH_NAME}/site/"
+test -d "/temp/$1/site" && rm -r "/temp/$1/site"
+mkdir "/temp/$1/site"
+cp "${WORKSPACE}/modules" "/temp/$1/site/"
+cp "${WORKSPACE}/site.mk" "/temp/$1/site/"
+cp "${WORKSPACE}/site.conf" "/temp/$1/site/"
 
 # Gluon Pakete aktualisieren und Build ausfuhren
-cd "/temp/${env.BRANCH_NAME}"
+cd "/temp/$1"
+
+#temporärer fix
+test -f "/temp/$1/openwrt/dl/linux-3.18.44.tar.xz" || wget https://www.kernel.org/pub/linux/kernel/v3.0/linux-3.18.44.tar.xz -O "/temp/$1/openwrt/dl/linux-3.18.44.tar.xz"
+
 make update "GLUON_RELEASE=$GLUON_RELEASE"
 make clean V=s GLUON_TARGET=ar71xx-generic
 make -j5 V=s GLUON_TARGET=ar71xx-generic GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
