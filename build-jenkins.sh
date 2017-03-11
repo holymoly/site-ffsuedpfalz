7eb0e47da44fddd212e9e722d4fec9708166f44c
holymoly <holzmolz@googlemail.com>
2017-03-11 17:46:40 +0100

    Update build-jenkins.sh

--- build-jenkins.sh
+++ build-jenkins.sh
@@ -50,6 +50,14 @@ test -f "/temp/$1/openwrt/dl/linux-3.18.44.tar.xz" || wget https://www.kernel.or
 make update "GLUON_RELEASE=$GLUON_RELEASE"
 make clean V=s GLUON_TARGET=ar71xx-generic
 make -j5 V=s GLUON_TARGET=ar71xx-generic GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=ar71xx-tiny GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=ar71xx-nand GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=brcm2708-bcm2708 GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=brcm2708-bcm2709 GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=mpc85xx-generic GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=x86-generic GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=x86-geode GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
+make -j5 V=s GLUON_TARGET=x86-64 GLUON_BRANCH=experimental "GLUON_RELEASE=$GLUON_RELEASE"
 # make V=s "GLUON_RELEASE=$GLUON_RELEASE" "GLUON_BRANCH=stable"
 cp -r "/temp/$1/output" "${WORKSPACE}/"
 # Manifest für Autoupdater erstellen und mit den Key des Servers unterschreiben
