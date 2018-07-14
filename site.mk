6580c5e6dae82f885e77ded046517d7d9210a62f
Maurice Streubel <holymoly@holybook.local>
2018-07-14 16:33:09 +0200

    update

--- site.mk
+++ site.mk
@@ -1,65 +1,72 @@
-GLUON_FEATURES := \
-	autoupdater \
-	alfred \
-	ebtables-filter-multicast \
-	ebtables-filter-ra-dhcp \
-	ebtables-limit-arp \
-	ebtables-source-filter \
-	mesh-batman-adv-15 \
-	mesh-vpn-tunneldigger \
-	radvd \
-	respondd \
-	status-page \
-	web-advanced \
-	web-wizard
-
 GLUON_SITE_PACKAGES := \
-	gluon-autorestart \
-	gluon-tunneldigger-watchdog \
-	iwinfo \
-	haveged
+  gluon-mesh-batman-adv-15 \
+  gluon-alfred \
+  gluon-respondd \
+  gluon-autorestart \
+  gluon-autoupdater \
+  gluon-config-mode-autoupdater \
+  gluon-config-mode-contact-info \
+  gluon-config-mode-core \
+  gluon-config-mode-geo-location \
+  gluon-config-mode-hostname \
+  gluon-config-mode-mesh-vpn \
+  gluon-ebtables-filter-multicast \
+  gluon-ebtables-filter-ra-dhcp \
+  gluon-ebtables-source-filter \
+  gluon-web-admin \
+  gluon-web-autoupdater \
+  gluon-web-wifi-config \
+  gluon-web-private-wifi \
+  gluon-web-network \
+  gluon-mesh-vpn-tunneldigger \
+  gluon-tunneldigger-watchdog \
+  gluon-radvd \
+  gluon-setup-mode \
+  gluon-status-page \
+  iwinfo \
+  haveged
 
 USB_BASIC := \
-	kmod-usb-core \
-	kmod-usb2 \
-	kmod-usb-hid
+  kmod-usb-core \
+  kmod-usb2 \
+  kmod-usb-hid
 
 USB_NIC := \
-	kmod-usb-net \
-	kmod-usb-net-asix \
-	kmod-usb-net-rtl8150 \
-	kmod-usb-net-rtl8152 \
-	kmod-usb-net-dm9601-ether
+  kmod-usb-net \
+  kmod-usb-net-asix \
+  kmod-usb-net-rtl8150 \
+  kmod-usb-net-rtl8152 \
+  kmod-usb-net-dm9601-ether
 
 USB_WIFI := \
-	kmod-rtl8192cu
+  kmod-rtl8192cu
 
 ifeq ($(GLUON_TARGET),x86-generic)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		kmod-usb-ohci-pci \
-		$(USB_NIC)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    kmod-usb-ohci-pci \
+    $(USB_NIC)
 endif
 
 ifeq ($(GLUON_TARGET),x86-64)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		$(USB_NIC) \
-		kmod-igb #APU2
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    kmod-igb #APU2
 endif
 
 ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		$(USB_NIC) \
-		$(USB_WIFI)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    $(USB_WIFI)
 endif
 
 ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		$(USB_NIC) \
-		$(USB_WIFI)
+  GLUON_SITE_PACKAGES += \
+    $(USB_BASIC) \
+    $(USB_NIC) \
+    $(USB_WIFI)
 endif
 
 DEFAULT_GLUON_RELEASE := v1.6.X-exp-$(shell date '+%Y%m%d')
