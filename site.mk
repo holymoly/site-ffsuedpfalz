49849126f11ad3839718651ad692bab9e16844fd
holymoly <holzmolz@googlemail.com>
2018-08-26 06:31:34 +0200

    gluon-radv-filterd is not supported by 2017.1.x

--- site.mk
+++ site.mk
@@ -21,7 +21,6 @@ GLUON_SITE_PACKAGES := \
   gluon-mesh-vpn-tunneldigger \
   gluon-tunneldigger-watchdog \
   gluon-radvd \
-  gluon-radv-filterd \
   gluon-setup-mode \
   gluon-status-page \
   iwinfo \
